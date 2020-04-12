9963dc6c3329b43ecd569b0b4c4097e80958678f
john <johnj@unicourt.com>
2020-04-13 03:07:11 +0530

    path

--- dev-portal/src/pages/Apis.jsx
+++ dev-portal/src/pages/Apis.jsx
@@ -4,7 +4,7 @@
 import React from 'react'
 
 // swagger-ui
-import SwaggerUI from './swagger-3.23.8'
+import SwaggerUI from '../../swagger-3.23.8'
 import 'swagger-ui/dist/swagger-ui.css'
 
 // semantic-ui
