e76b676de7765c758499002f358f36b4a6b9446a
john <johnj@unicourt.com>
2020-07-16 03:49:10 +0530

    Export Option

--- dev-portal/src/components/GetSdk.jsx
+++ dev-portal/src/components/GetSdk.jsx
@@ -468,7 +468,7 @@ function downloadAPISpecFile (dataUri, fileName, ext) {
   var dataStr = ""
   // adding this code because the URL in the swagger documentation was comming with // which fails while trying the API's 
   if (dataUri["servers"]) {
-    for (i =0;i < dataUri["servers"].length; i++) {
+    for (var i =0; i < dataUri["servers"].length; i++) {
        if (dataUri["servers"][i]["variables"] && dataUri["servers"][i]["variables"]["basePath"]) {
            if (dataUri["servers"][i]["variables"]["basePath"]["default"][0] ==="/") {
                var correctedUrl = dataUri["servers"][i]["url"]
